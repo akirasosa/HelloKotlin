4c0df10ce83c7c7396090c633ac384d409b6b8ca
Akira Sosa <akirasosa@gmail.com>
2014-07-13 16:38:12 +0700

    initial commit

---
+++ app/src/main/kotlin/com/example/akira/hellokotlin/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.akira.hellokotlin
+
+import android.app.Activity
+import android.os.Bundle
+
+public class MainActivity : Activity() {
+
+    protected override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my)
+    }
+
+
+}
